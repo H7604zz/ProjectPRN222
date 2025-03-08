12aee16f540ec9f0a3e9d0103ed8ebfa0462e1a5
hoa <hoaphung10112003@gmail.com>
2025-03-08 12:45:00 +0700

    pagging manage user

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,22 +8,38 @@ namespace ProjectPrn222.Controllers
 	//[Authorize(Roles = "Admin")]
 	public class AdminController : Controller
 	{
-		private readonly IUserService _userService;
-		public AdminController(IUserService userService)
+		private readonly IUserService _userService; 
+        private readonly int ITEM_PER_PAGE = 10;
+        public AdminController(IUserService userService)
 		{
 			_userService = userService;
 		}
 
-		public ActionResult ManageUser(string? keyword)
+		public ActionResult ManageUser(string? keyword, int currentPage)
 		{
             var listUserQuery = !string.IsNullOrEmpty(keyword)
                 ? _userService.SearchUser(keyword)
                 : _userService.GetAllUsers()
-				.ToList().Where(u => u.RoleName != "Admin"); 
+                .ToList().Where(u => u.RoleName != "Admin");
+
+            int totalProduct = listUserQuery.Count(); 
+            int totalPage = (int)Math.Ceiling((double)totalProduct / ITEM_PER_PAGE); // Tổng số trang
+
+            // Đảm bảo currentPage trong khoảng hợp lệ
+            currentPage = Math.Max(1, Math.Min(currentPage, totalPage));
 
             ViewBag.keyword = keyword;
+            ViewBag.CurrentPage = currentPage;
+            ViewBag.CountPage = totalPage;
+
+            // Phân trang
+            listUserQuery = listUserQuery
+                .Skip((currentPage - 1) * ITEM_PER_PAGE)
+                .Take(ITEM_PER_PAGE)
+                .ToList();
 
             return View(listUserQuery);
+
 		}
 	}
 }
