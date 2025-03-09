fe7c05e99746ab46a35636402c9d883f65b1d851
hoa <hoaphung10112003@gmail.com>
2025-03-09 23:07:20 +0700

    add field isactive in Category model

---
+++ Migrations/20250309160400_addFieldIsActiveInCategory.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectPrn222.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFieldIsActiveInCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "ProductPrices",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "FinalTotal",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "DiscountAmount",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Subtotal",
+                table: "OrderDetails",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "OrderDetails",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Discount",
+                table: "OrderDetails",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ProductPrices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FinalTotal",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Subtotal",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "OrderDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
