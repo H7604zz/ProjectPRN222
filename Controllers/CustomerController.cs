72c2eb63ffcd473f4af28e69ea725ec2f5276164
hoa <hoaphung10112003@gmail.com>
2025-03-09 23:30:06 +0700

    view cart in role Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectPrn222.Models;
+
+namespace ProjectPrn222.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+            
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
