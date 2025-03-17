01acfcddb55df882a2e85b1fcb5b9dc539b8342c
hoa <hoaphung10112003@gmail.com>
2025-03-17 17:08:00 +0700

    checkout with vnpay

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectPrn222.Models.VNPay;
+using ProjectPrn222.Service.Iterface;
+
+namespace ProjectPrn222.Controllers
+{
+	[Authorize(Roles = "Customer")]
+	public class PaymentController : Controller
+	{
+		private readonly IVnPayService _vnPayService;
+		public PaymentController(IVnPayService vnPayService)
+		{
+
+			_vnPayService = vnPayService;
+		}
+		[HttpPost]
+		public IActionResult CreatePaymentUrlVnpay(PaymentInformationModel model)
+		{
+			var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+
+			return Redirect(url);
+		}
+		[HttpGet]
+		public IActionResult PaymentCallbackVnpay()
+		{
+			var response = _vnPayService.PaymentExecute(Request.Query);
+
+			return Json(response);
+		}
+
+	}
+}
