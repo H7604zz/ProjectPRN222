f1a12ff6cb1e0811e3e5acc6dd21b842f64ce251
hoa <hoaphung10112003@gmail.com>
2025-03-23 18:24:18 +0700

    add current price in order details

---
+++ Migrations/20250323105500_addCurrentPriceInOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectPrn222.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCurrentPriceInOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "OrderDetails",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "OrderDetails");
+        }
+    }
+}
--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -12,8 +12,8 @@ public partial class OrderDetail
     public int ProductId { get; set; }
 
     public int Quantity { get; set; }
-
+    public float Price { get; set; }
     public virtual Order Order { get; set; } = null!;
-
     public virtual Product Product { get; set; } = null!;
+
 }
